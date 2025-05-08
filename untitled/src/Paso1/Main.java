fe8e0c9ee1d2f3dbb12bf4d8a9f499336f95568f
eddymap <eddy.armas@udla.edu.ec>
2025-05-08 16:38:56 -0500

    tercer commit con arreglos

--- untitled/src/Paso1/Main.java
+++ untitled/src/Paso1/Main.java
@@ -3,6 +3,12 @@ package Paso1;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        String[] meses = {"Enero","Febrero","Marzo","Abril","Mayo","Junio",
+        "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+
+        int[] lluvia = new int[12];
+        lluvia[0]=23; lluvia[1]=24; lluvia[2]=20; lluvia[3]=21; lluvia[4]=26;
+        lluvia[5]=22; lluvia[6]=23; lluvia[7]=23; lluvia[8]=22; lluvia[9]=25;
+        lluvia[10]=22; lluvia[11]=20;
     }
 }
